a7e25994c2a2f5e35590a8079aaf428f073893fe
o7digital <101292703+o7digital@users.noreply.github.com>
2026-05-31 10:40:52 -0600

    Disable Clerk middleware until Vercel keys are configured

--- apps/frontend/middleware.ts
+++
@@ -1,9 +0,0 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
-
-const isPublicRoute = createRouteMatcher(["/"]);
-
-export default clerkMiddleware((auth, req) => {
-  if (!isPublicRoute(req)) {
-    auth.protect();
-  }
-});
